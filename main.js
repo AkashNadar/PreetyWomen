be629662b1bbd61284c3b1a2635f6e9449d784a7
AkashNadar <Akashnadar14@gmail.com>
2026-08-24 23:03:58 +0530

    first commit

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ const imageOne = document.querySelector(".image-1");
 const imageTwo = document.querySelector(".image-2");
 const btnYes = document.querySelector(".btn-yes");
 const btnNo = document.querySelector(".btn-no");
+const frtImg = document.querySelector(".frt-img");
+const mySong = document.getElementById("mySong");
+const preetywomen = document.querySelector(".preetywomen");
+
 
 function getRandomNumber(min, max) {
   // Calculate the random number between min and max (inclusive)
@@ -33,8 +37,86 @@ btnNo.addEventListener("mouseover", (event) => {
   btnNo.style.left = Math.floor(newLeft) + "px";
 });
 
+btnNo.addEventListener("pointerenter", moveNoButton);
+btnNo.addEventListener("pointerdown", (event) => {
+    event.preventDefault();
+    moveNoButton();
+});
+
+function moveNoButton() {
+    const containerWidth = container.clientWidth;
+    const containerHeight = container.clientHeight;
+
+    const btnWidth = btnNo.offsetWidth;
+    const btnHeight = btnNo.offsetHeight;
+
+    const newLeft = getRandomNumber(
+        0,
+        containerWidth - btnWidth
+    );
+
+    const newTop = getRandomNumber(
+        0,
+        containerHeight - btnHeight
+    );
+
+    btnNo.style.left = `${newLeft}px`;
+    btnNo.style.top = `${newTop}px`;
+}
+
+function createSunflower() {
+    const container = document.getElementById("sunflower-container");
+
+    const sunflower = document.createElement("img");
+
+    sunflower.src = "assets/sunflower.png";
+    sunflower.classList.add("sunflower");
+
+    // Random position
+    sunflower.style.left = Math.random() * 100 + "vw";
+
+    // Random size
+    const size = Math.random() * 25 + 25;
+    sunflower.style.width = size + "px";
+
+    // Random falling speed
+    const duration = Math.random() * 3 + 4;
+    sunflower.style.animationDuration = duration + "s";
+
+    container.appendChild(sunflower);
+
+    // Remove after animation
+    setTimeout(() => {
+        sunflower.remove();
+    }, duration * 1000);
+}
+
+function startSunflowers() {
+    const shower = setInterval(() => {
+        createSunflower();
+    }, 150);
+
+    // 🌻 Shower stops after 3 seconds
+    setTimeout(() => {
+        clearInterval(shower);
+
+        // Wait 3 seconds, then shower again
+        setTimeout(() => {
+            sunflowerShower();
+        }, 8000);
+
+    }, 10000);
+}
+
 btnYes.addEventListener("click", (e) => {
   btnNo.classList.add("hide");
   imageOne.classList.add("hide");
   imageTwo.classList.remove("hide");
+  frtImg.src = "assets/secondImg.png"
+  btnYes.classList.add("hide");
+  mySong.currentTime = 0;
+  mySong.play();
+  preetywomen.classList.remove("hide");
+  preetywomen.classList.add("fade-in");
+  startSunflowers();
 });
